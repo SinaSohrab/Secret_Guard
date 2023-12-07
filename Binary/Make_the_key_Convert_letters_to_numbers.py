4f2a18ce607684b4034f3abf112ec70d27e91372
SinaSohrab <sinasohrab84@gmail.com>
2023-12-07 13:42:17 +0330

    fixe code Mtkcltn.py

--- Binary/Make_the_key_Convert_letters_to_numbers.py
+++ Binary/Make_the_key_Convert_letters_to_numbers.py
@@ -1,14 +1,15 @@
 import random
 from pickle import dump
-letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYX"
 
-r1=list(letters)
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYX"
+
+r1 = list(letters)
 random.shuffle(r1)
-#r1="".join(r1)
+# r1="".join(r1)
 print(r1)
-index_letters=r1.index("q")
+index_letters = r1.index("q")
 
-f=open("Binary/password/key.letter","wb")
-dump((r1),f)
+f = open("Binary/password/key.letter", "wb")
+dump((r1), f)
 f.close()
-print(index_letters)
+# print(index_letters)
