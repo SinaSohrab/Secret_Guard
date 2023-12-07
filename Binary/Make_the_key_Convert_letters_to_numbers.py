5f01900f3299237b008433d9e07f8cec1cafa120
SinaSohrab <sinasohrab84@gmail.com>
2023-12-07 13:44:55 +0330

    save project

--- Binary/Make_the_key_Convert_letters_to_numbers.py
+++ Binary/Make_the_key_Convert_letters_to_numbers.py
@@ -4,12 +4,9 @@ from pickle import dump
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYX"
 
 r1 = list(letters)
+
 random.shuffle(r1)
-# r1="".join(r1)
-print(r1)
-index_letters = r1.index("q")
 
 f = open("Binary/password/key.letter", "wb")
 dump((r1), f)
 f.close()
-# print(index_letters)
